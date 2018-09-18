def2c3d8d15b61a2346618794ba64e21aa1a9f51
Mikkel Ziemer <mzhjensen@gmail.com>
2018-09-18 11:51:47 +0200

    Merged

--- 03 Sets/03sets/src/main/java/sets/Main.java
+++ 03 Sets/03sets/src/main/java/sets/Main.java
@@ -1,8 +1,38 @@
 package sets;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
+        Set<Integer> members = new HashSet<>();
+        members.add(1);
+        members.add(2);
+        members.add(3);
+        Sets intSet = new SetsImpl<Integer>(false, false,members);
 
+        Set<Integer> secondMembers = new HashSet<>();
+        secondMembers.add(3);
+        secondMembers.add(4);
+        secondMembers.add(5);
+
+        Sets<Integer> secondSet = new SetsImpl(true, false, secondMembers);
+
+        Sets unionedSet = intSet.union(secondSet);
+
+        unionedSet.printFormatted();
+
+        Sets intersection = intSet.intersection(secondSet);
+        intersection.printFormatted();
+
+        Set<Integer> thirdMembers = new HashSet<>();
+        thirdMembers.add(1);
+        thirdMembers.add(2);
+        thirdMembers.add(3);
+        thirdMembers.add(4);
+
+        Sets<Integer> thirdSet = new SetsImpl(thirdMembers);
+        System.out.println(intSet.compareTo(thirdSet));
     }
 }
